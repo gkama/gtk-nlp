65f7bea01cd9c6919d5275afc24c15d1686d5d9e
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-19 12:19:37 -0500

    add initial unit test project and needed packages

---
+++ nlp.tests/NlpRepository.Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+using Xunit;
+using Moq;
+
+using nlp.data;
+using nlp.services;
+
+namespace nlp.tests
+{
+    public class NlpRepositoryTests
+    {
+        private readonly ILogger<NlpRepository> _mockLogger;
+
+        public NlpRepositoryTests()
+        {
+            _mockLogger = Mock.Of<ILogger<NlpRepository>>();
+        }
+    }
+}
