33326513eda607d980d6393ab58262da6ec8f6bf
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-19 13:36:27 -0500

    add model settings

---
+++ nlp.data/ModelSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace nlp.data
+{
+    public class ModelSettings
+    {
+        public string[] StopWords { get; set; }
+        public char[] Delimiters { get; set; }
+
+        public int? StopWordsLength => StopWords?.Sum(x => x.Length);
+    }
+}
