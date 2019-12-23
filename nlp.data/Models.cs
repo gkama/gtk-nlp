c78d57605ab38757697b2047bb23ee3220ba1ea8
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-23 15:26:44 -0500

    add static models

---
+++ nlp.data/Models.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nlp.data
+{
+    public static class Models
+    {
+        public static IModel<Model> Vanguard =>
+            new Model()
+            {
+                Id = "984ce69d-de79-478b-9223-ff6349514e19",
+                Name = "Vanguard",
+                Children = {
+                    new Model()
+                    {
+                        Id = "5ec6957d-4de7-4199-9373-d4a7fb59d6e1",
+                        Name = "Index Funds",
+                        Details = "vbiix|vbinx|vbisx|vbltx|vbmfx|vdaix|vdvix|veiex|veurx|vexmx|vfinx|vfsvx|vftsx|vfwix|vgovx|vgtsx|vhdyx|viaix|vigrx|vihix|vimsx|visgx|visvx|vivax|vlacx|vmgix|vmvix|vpacx|vtebx|vtibx|vtipx|vtsax|vtsmx|vtws"
+                    }
+                }
+            };
+    }
+}
