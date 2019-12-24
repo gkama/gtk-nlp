b49be071fb85a8d0c1d893d1c958c846503c54fc
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-24 16:12:41 -0500

    add json property name to model settings

--- nlp.data/ModelSettings.cs
+++ nlp.data/ModelSettings.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json.Serialization;
 
 namespace nlp.data
 {
     public class ModelSettings<T> : IModelSettings<T>
         where T : IModel<T>
     {
+        [JsonPropertyName("id")]
         public string Id { get; set; }
-        public T Model { get; set; }     
+
+        [JsonPropertyName("model")]
+        public T Model { get; set; }
+
+        [JsonPropertyName("stopWords")]
         public string[] StopWords { get; set; }
+
+        [JsonPropertyName("stopWordsLength")]
         public int? StopWordsLength => StopWords?.Sum(x => x.Length);
+
+        [JsonPropertyName("delimiters")]
         public string[] Delimiters { get; set; }
+
+        [JsonPropertyName("publickey")]
         public Guid PublicKey => Guid.NewGuid();
     }
 }
