c97914f59763ee3df67da54a8d6e718be7464470
gkama <kamacharov.gk@pg.com>
2019-12-25 14:32:35 -0500

    add public to all IModel<T> properties

--- nlp.data/IModel.cs
+++ nlp.data/IModel.cs
@@ -7,18 +7,18 @@ namespace nlp.data
     public interface IModel<T>
     {
         [JsonPropertyName("id")]
-        string Id { get; set; }
+        public string Id { get; set; }
 
         [JsonPropertyName("name")]
-        string Name { get; set; }
+        public string Name { get; set; }
 
         [JsonPropertyName("details")]
-        string Details { get; set; }
+        public string Details { get; set; }
 
         [JsonPropertyName("publickey")]
         public Guid PublicKey { get; }
 
         [JsonPropertyName("children")]
-        ICollection<T> Children { get; set; }
+        public ICollection<T> Children { get; set; }
     }
 }
