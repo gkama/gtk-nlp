7ad95cf89ddf4ff9eedb9a6170b9ebb22b3bd44f
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-26 11:33:33 -0500

    add to object<T> extension method unit test

--- nlp.tests/ExtensionMethods.Tests.cs
+++ nlp.tests/ExtensionMethods.Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -15,5 +16,19 @@ namespace nlp.tests
 {
     public class ExtensionMethodsTests
     {
+        public ExtensionMethodsTests()
+        {
+
+        }
+
+        [Fact]
+        public void ToObjectT_Valid()
+        {
+            var jsonStr = "{ \"id\": \"1\", \"name\": \"test\", \"details\": \"test,1,2,3\", \"children\": [] }";
+            var json = JsonDocument.Parse(jsonStr).RootElement;
+
+            //Asserts
+            Assert.NotNull(json.ToObject<Model>());
+        }
     }
 }
