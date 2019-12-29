0b2fa0399d20e01c2a13796a13c94e3a6567f776
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-29 14:35:20 -0500

    add category class and interface

---
+++ nlp.data/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nlp.data
+{
+    public class Category
+    {
+        public string Name { get; set; }
+        public int TotalWeight => Matched.Sum(x => x.Weight);
+        public ICollection<IMatched> Matched { get; set; } = new List<IMatched>();
+    }
+
+    public class Matched : IMatched
+    {
+        public string Value { get; set; }
+        public int Weight { get; set; } = 0;
+    }
+}
---
+++ nlp.data/ICategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace nlp.data
+{
+    public interface ICategory
+    {
+        public string Mame { get; set; }
+        public int TotalWeight { get; }
+        public ICollection<IMatched> Matched { get; set; }
+    }
+
+    public interface IMatched
+    {
+        public string Value { get; set; }
+        public int Weight { get; set; }
+    }
+}
