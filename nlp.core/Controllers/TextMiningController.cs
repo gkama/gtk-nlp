84e877cc9e083016aab19840d124b1024ac37064
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-30 17:01:22 -0500

    add text mining controller

---
+++ nlp.core/Controllers/TextMiningController.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using nlp.data;
+using nlp.services;
+
+namespace nlp.core.Controllers
+{
+    [Route("text/mine")]
+    [ApiController]
+    public class TextMiningController : ControllerBase
+    {
+        private readonly ITextMiningRepository<Model> _repo;
+
+        public TextMiningController(ITextMiningRepository<Model> repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost]
+        public IActionResult Mine([FromBody]dynamic Request)
+        {
+            return Ok(_repo.Mine(((JsonElement)Request).GetProperty("content").GetString()));
+        }
+    }
+}
