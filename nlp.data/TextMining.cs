3d3f9a2e89ac86a516c187381d0d14d2beb30241
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-31 09:56:44 -0500

    update Stem() method to be per Word (string) and not a list

--- nlp.data/TextMining.cs
+++ nlp.data/TextMining.cs
@@ -16,7 +16,7 @@ namespace nlp.data
         /// For more information, see Manning and Schütze (2002).
         /// https://en.wikipedia.org/wiki/Stemming
         /// </summary>
-        public static List<string> Stem(this List<string> Text)
+        public static string Stem(this string Word)
         {
             /*
              Suffix-stripping algorithms
@@ -25,77 +25,69 @@ namespace nlp.data
                 if the word ends in 'ing', remove the 'ing'
                 if the word ends in 'ly', remove the 'ly'
             */
-            var TextToReturn = new List<string>();
-
-            foreach (var word in Text)
-            {
-                if (string.IsNullOrWhiteSpace(word) || word.Length <= 2)
-                { TextToReturn.Add(word); continue; }
+            if (string.IsNullOrWhiteSpace(Word) || Word.Length <= 2)
+                return Word;
 
                 //Step 1: get rid of plurals and -ed or -ing
-                if (word.EndsWith("'")) { TextToReturn.Add(word.RemoveLast(1)); continue; }
-                if (word.EndsWith("ies")
-                    || word.EndsWith("sses")
-                    || word.EndsWith("ed")
-                    || word.EndsWith("ly")
-                    || word.EndsWith("'s")) { TextToReturn.Add(word.RemoveLast(2)); continue; }
-                if (word.EndsWith("ing")
-                    || word.EndsWith("'s'")) { TextToReturn.Add(word.RemoveLast(3)); continue; }
+                if (Word.EndsWith("'")) { return Word.RemoveLast(1); }
+                if (Word.EndsWith("ies")
+                    || Word.EndsWith("sses")
+                    || Word.EndsWith("ed")
+                    || Word.EndsWith("ly")
+                    || Word.EndsWith("'s")) return Word.RemoveLast(2);
+                if (Word.EndsWith("ing")
+                    || Word.EndsWith("'s'")) Word.RemoveLast(3);
 
                 //Step3: maps double suffices to single ones. so -ization ( = -ize plus
                 //-ation) maps to -ize etc. note that the string before the suffix must give m() > 0.
-                if (word.EndsWith("ational")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ate")); continue; }
-                if (word.EndsWith("tional")) { TextToReturn.Add(word.ReplaceEndIndex(6, "tion")); continue; }
-                if (word.EndsWith("enci")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ence")); continue; }
-                if (word.EndsWith("anci")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ance")); continue; }
-                if (word.EndsWith("izer")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ize")); continue; }
-                if (word.EndsWith("bli")) { TextToReturn.Add(word.ReplaceEndIndex(3, "ble")); continue; }
-                if (word.EndsWith("alli")) { TextToReturn.Add(word.ReplaceEndIndex(4, "al")); continue; }
-                if (word.EndsWith("entli")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ent")); continue; }
-                if (word.EndsWith("eli")) { TextToReturn.Add(word.ReplaceEndIndex(3, "e")); continue; }
-                if (word.EndsWith("ousli")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ous")); continue; }
-                if (word.EndsWith("ization")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ize")); continue; }
-                if (word.EndsWith("ation")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ate")); continue; }
-                if (word.EndsWith("ator")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ate")); continue; }
-                if (word.EndsWith("alism")) { TextToReturn.Add(word.ReplaceEndIndex(5, "al")); continue; }
-                if (word.EndsWith("iveness")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ive")); continue; }
-                if (word.EndsWith("fulness")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ful")); continue; }
-                if (word.EndsWith("ousness")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ous")); continue; }
-                if (word.EndsWith("aliti")) { TextToReturn.Add(word.ReplaceEndIndex(5, "al")); continue; }
-                if (word.EndsWith("iviti")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ive")); continue; }
-                if (word.EndsWith("biliti")) { TextToReturn.Add(word.ReplaceEndIndex(6, "ble")); continue; }
-                if (word.EndsWith("logi")) { TextToReturn.Add(word.ReplaceEndIndex(4, "log")); continue; }
+                if (Word.EndsWith("ational")) return Word.ReplaceEndIndex(7, "ate");
+                if (Word.EndsWith("tional")) Word.ReplaceEndIndex(6, "tion");
+                if (Word.EndsWith("enci")) Word.ReplaceEndIndex(4, "ence");
+                if (Word.EndsWith("anci")) Word.ReplaceEndIndex(4, "ance");
+                if (Word.EndsWith("izer")) return Word.ReplaceEndIndex(4, "ize");
+                if (Word.EndsWith("bli")) return Word.ReplaceEndIndex(3, "ble");
+                if (Word.EndsWith("alli")) return Word.ReplaceEndIndex(4, "al");
+                if (Word.EndsWith("entli")) return Word.ReplaceEndIndex(5, "ent");
+                if (Word.EndsWith("eli")) return Word.ReplaceEndIndex(3, "e");
+                if (Word.EndsWith("ousli")) return Word.ReplaceEndIndex(5, "ous");
+                if (Word.EndsWith("ization")) return Word.ReplaceEndIndex(7, "ize");
+                if (Word.EndsWith("ation")) return Word.ReplaceEndIndex(5, "ate");
+                if (Word.EndsWith("ator")) return Word.ReplaceEndIndex(4, "ate");
+                if (Word.EndsWith("alism")) return Word.ReplaceEndIndex(5, "al");
+                if (Word.EndsWith("iveness")) return Word.ReplaceEndIndex(7, "ive");
+                if (Word.EndsWith("fulness")) return Word.ReplaceEndIndex(7, "ful");
+                if (Word.EndsWith("ousness")) return Word.ReplaceEndIndex(7, "ous");
+                if (Word.EndsWith("aliti")) return Word.ReplaceEndIndex(5, "al");
+                if (Word.EndsWith("iviti")) return Word.ReplaceEndIndex(5, "ive");
+                if (Word.EndsWith("biliti")) return Word.ReplaceEndIndex(6, "ble");
+                if (Word.EndsWith("logi")) return Word.ReplaceEndIndex(4, "log");
 
                 //Step 4: deals with -ic-, -full, -ness etc. similar strategy to step3
-                if (word.EndsWith("icate")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ic")); continue; }
-                if (word.EndsWith("ative")) { TextToReturn.Add(word.ReplaceEndIndex(5, "")); continue; }
-                if (word.EndsWith("alize")) { TextToReturn.Add(word.ReplaceEndIndex(5, "al")); continue; }
-                if (word.EndsWith("iciti")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ic")); continue; }
-                if (word.EndsWith("ical")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ic")); continue; }
-                if (word.EndsWith("ful")) { TextToReturn.Add(word.ReplaceEndIndex(3, "")); continue; }
-                if (word.EndsWith("ness")) { TextToReturn.Add(word.ReplaceEndIndex(4, "")); continue; }
+                if (Word.EndsWith("icate")) return Word.ReplaceEndIndex(5, "ic");
+                if (Word.EndsWith("ative")) return Word.ReplaceEndIndex(5, "");
+                if (Word.EndsWith("alize")) return Word.ReplaceEndIndex(5, "al");
+                if (Word.EndsWith("iciti")) return Word.ReplaceEndIndex(5, "ic");
+                if (Word.EndsWith("ical")) return Word.ReplaceEndIndex(4, "ic");
+                if (Word.EndsWith("ful")) return Word.ReplaceEndIndex(3, "");
+                if (Word.EndsWith("ness")) return Word.ReplaceEndIndex(4, "");
 
                 //Step 5:
-                if (word.EndsWith("ence")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ence", ""); continue; }
-                if (word.EndsWith("er")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "er", ""); continue; }
-                if (word.EndsWith("ic")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ic", ""); continue; }
-                if (word.EndsWith("able")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "able", ""); continue; }
-                if (word.EndsWith("ant")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ant", ""); continue; }
-                if (word.EndsWith("ement")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ement", ""); continue; }
-                if (word.EndsWith("ent")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ent", ""); continue; }
-                if (word.EndsWith("ou")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ou", ""); continue; }
-                if (word.EndsWith("ism")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ism", ""); continue; }
-                if (word.EndsWith("ate")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ate", ""); continue; }
-                if (word.EndsWith("iti")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "iti", ""); continue; }
-                if (word.EndsWith("ous")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ous", ""); continue; }
-                if (word.EndsWith("ive")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ive", ""); continue; }
-                if (word.EndsWith("ize")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ize", ""); continue; }
+                //if (Word.EndsWith("ence")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ence", ""); continue; }
+                //if (Word.EndsWith("er")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "er", ""); continue; }
+                //if (Word.EndsWith("ic")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ic", ""); continue; }
+                //if (Word.EndsWith("able")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "able", ""); continue; }
+                //if (Word.EndsWith("ant")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ant", ""); continue; }
+                //if (Word.EndsWith("ement")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ement", ""); continue; }
+                //if (Word.EndsWith("ent")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ent", ""); continue; }
+                //if (Word.EndsWith("ou")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ou", ""); continue; }
+                //if (Word.EndsWith("ism")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ism", ""); continue; }
+                //if (Word.EndsWith("ate")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ate", ""); continue; }
+                //if (Word.EndsWith("iti")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "iti", ""); continue; }
+                //if (Word.EndsWith("ous")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ous", ""); continue; }
+                //if (Word.EndsWith("ive")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ive", ""); continue; }
+                //if (Word.EndsWith("ize")) { TextToReturn.ReplaceAndAddIfEndsWith(word, "ize", ""); continue; }
 
-                //Step 6: if all passes, then simply add the word
-                TextToReturn.Add(word);
-            };
-
-            return TextToReturn;
+            return Word;
         }
 
         public static string RemoveLast(this string Str, int Characters)
@@ -106,12 +98,6 @@ namespace nlp.data
         public static string ReplaceEndIndex(this string Str, int EndIndex, string Replacement)
         {
             return $"{Str.Substring(0, Str.Length - EndIndex)}{Replacement}";
-        }
-
-        public static void ReplaceAndAddIfEndsWith(this List<string> Text, string Word, string EndsWith, string Replacement)
-        {
-            if (Word.EndsWith(EndsWith))
-                Text.Add(Word.ReplaceEndIndex(EndsWith.Length, Replacement));
         }
 
         public static bool IsConsonant(char Letter)
