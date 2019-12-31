e9f5726ad6951baa2e586b9768d2f2ce8628c79b
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-31 11:03:26 -0500

    add bulgarian controller

---
+++ nlp.core/Controllers/BulgarianController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using nlp.data;
+using nlp.services;
+
+namespace nlp.core.Controllers
+{
+    [ApiController]
+    [Route("nlp/bulgarian")]
+    public class BulgarianController : ControllerBase
+    {
+        private readonly INlpRepository<BulgarianModel> _repo;
+
+        public BulgarianController(INlpRepository<BulgarianModel> repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpPost]
+        [Route("categorize/{id}")]
+        public IActionResult CategorizeWithModelId([FromRoute]string id, [FromBody]dynamic request)
+        {
+            return Ok(_repo.Categorize(request, id));
+        }
+    }
+}
