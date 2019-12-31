676b45fa4d32afe40b5f1af4a677905f19a1329f
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-30 21:08:54 -0500

    update the stemming algorithm

--- nlp.data/TextMining.cs
+++ nlp.data/TextMining.cs
@@ -33,39 +33,49 @@ namespace nlp.data
                 { TextToReturn.Add(word); continue; }
 
                 //Step 1: get rid of plurals and -ed or -ing
-                if (word.EndsWith("'"))
-                { TextToReturn.Add(word.RemoveLast(1)); continue; }
-                else if (word.EndsWith("ies")
+                if (word.EndsWith("'")) { TextToReturn.Add(word.RemoveLast(1)); continue; }
+                if (word.EndsWith("ies")
                     || word.EndsWith("sses")
                     || word.EndsWith("ed")
                     || word.EndsWith("ly")
-                    || word.EndsWith("'s"))
-                { TextToReturn.Add(word.RemoveLast(2)); continue; }
-                else if (word.EndsWith("ing")
-                    || word.EndsWith("'s'"))
-                { TextToReturn.Add(word.RemoveLast(3)); continue; }
+                    || word.EndsWith("'s")) { TextToReturn.Add(word.RemoveLast(2)); continue; }
+                if (word.EndsWith("ing")
+                    || word.EndsWith("'s'")) { TextToReturn.Add(word.RemoveLast(3)); continue; }
+
+                //Step3: maps double suffices to single ones. so -ization ( = -ize plus
+                //-ation) maps to -ize etc. note that the string before the suffix must give m() > 0.
+                if (word.EndsWith("ational")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ate")); continue; }
+                if (word.EndsWith("tional")) { TextToReturn.Add(word.ReplaceEndIndex(6, "tion")); }
+                if (word.EndsWith("enci")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ence")); continue; }
+                if (word.EndsWith("anci")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ance")); continue; }
+                if (word.EndsWith("izer")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ize")); continue; }
+                if (word.EndsWith("bli")) { TextToReturn.Add(word.ReplaceEndIndex(3, "ble")); continue; }
+                if (word.EndsWith("alli")) { TextToReturn.Add(word.ReplaceEndIndex(4, "al")); continue; }
+                if (word.EndsWith("entli")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ent")); continue; }
+                if (word.EndsWith("eli")) { TextToReturn.Add(word.ReplaceEndIndex(3, "e")); continue; }
+                if (word.EndsWith("ousli")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ous")); continue; }
+                if (word.EndsWith("ization")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ize")); continue; }
+                if (word.EndsWith("ation")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ate")); continue; }
+                if (word.EndsWith("ator")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ate")); continue; }
+                if (word.EndsWith("alism")) { TextToReturn.Add(word.ReplaceEndIndex(5, "al")); continue; }
+                if (word.EndsWith("iveness")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ive")); continue; }
+                if (word.EndsWith("fulness")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ful")); continue; }
+                if (word.EndsWith("ousness")) { TextToReturn.Add(word.ReplaceEndIndex(7, "ous")); continue; }
+                if (word.EndsWith("aliti")) { TextToReturn.Add(word.ReplaceEndIndex(5, "al")); continue; }
+                if (word.EndsWith("iviti")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ive")); continue; }
+                if (word.EndsWith("biliti")) { TextToReturn.Add(word.ReplaceEndIndex(6, "ble")); continue; }
+                if (word.EndsWith("logi")) { TextToReturn.Add(word.ReplaceEndIndex(4, "log")); continue; }
 
                 //Step 4: deals with -ic-, -full, -ness etc. similar strategy to step3
-                switch (word.LastLetter())
-                {
-                    case 'e':
-                        if (word.EndsWith("icate")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ic")); break; }
-                        if (word.EndsWith("ative")) { TextToReturn.Add(word.ReplaceEndIndex(5, "")); break; }
-                        if (word.EndsWith("alize")) { TextToReturn.Add(word.ReplaceEndIndex(5, "al")); }
-                        break;
-                    case 'i':
-                        if (word.EndsWith("iciti")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ic")); }
-                        break;
-                    case 'l':
-                        if (word.EndsWith("ical")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ic")); break; }
-                        if (word.EndsWith("ful")) { TextToReturn.Add(word.ReplaceEndIndex(3, "")); }
-                        break;
-                    case 's':
-                        if (word.EndsWith("ness")) { TextToReturn.Add(word.ReplaceEndIndex(4, "")); }
-                        break;
-                }
+                if (word.EndsWith("icate")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ic")); continue; }
+                if (word.EndsWith("ative")) { TextToReturn.Add(word.ReplaceEndIndex(5, "")); continue; }
+                if (word.EndsWith("alize")) { TextToReturn.Add(word.ReplaceEndIndex(5, "al")); continue; }
+                if (word.EndsWith("iciti")) { TextToReturn.Add(word.ReplaceEndIndex(5, "ic")); continue; }
+                if (word.EndsWith("ical")) { TextToReturn.Add(word.ReplaceEndIndex(4, "ic")); continue; }
+                if (word.EndsWith("ful")) { TextToReturn.Add(word.ReplaceEndIndex(3, "")); continue; }
+                if (word.EndsWith("ness")) { TextToReturn.Add(word.ReplaceEndIndex(4, "")); continue; }
 
-                //If all passes, then simply add the word
+                //Step 5: if all passes, then simply add the word
                 TextToReturn.Add(word);
             };
 
@@ -75,10 +85,6 @@ namespace nlp.data
         public static string RemoveLast(this string Str, int Characters)
         {
             return Str.Remove(Str.Length - Characters);
-        }
-        public static char LastLetter(this string Str)
-        {
-            return Str[Str.Length - 1];
         }
 
         public static string ReplaceEndIndex(this string Str, int EndIndex, string Replacement)
