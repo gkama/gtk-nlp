1a5753c4cf8fddeb5ee7d865e68c154c327c1e5a
Georgi Kamacharov <gkamacharov@gmail.com>
2019-12-31 10:56:59 -0500

    add bulgarian model

---
+++ nlp.data/BulgarianModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nlp.data
+{
+    public class BulgarianModel : IModel<BulgarianModel>
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Language => "Български език";
+        public string Details { get; set; }
+        public Guid PublicKey => Guid.NewGuid();
+        public ICollection<BulgarianModel> Children { get; set; } = new List<BulgarianModel>();
+    }
+}
