8ac4905574dfd784d52c1076845813aac1d4f58f
Georgi Kamacharov <gkamacharov@gmail.com>
2020-01-04 12:03:37 -0500

    remove json property name from interfaces

--- nlp.data/IModel.cs
+++ nlp.data/IModel.cs
@@ -6,19 +6,10 @@ namespace nlp.data
 {
     public interface IModel<T>
     {
-        [JsonPropertyName("id")]
         public string Id { get; set; }
-
-        [JsonPropertyName("name")]
         public string Name { get; set; }
-
-        [JsonPropertyName("details")]
         public string Details { get; set; }
-
-        [JsonPropertyName("publickey")]
         public Guid PublicKey { get; }
-
-        [JsonPropertyName("children")]
         public ICollection<T> Children { get; set; }
     }
 }
--- nlp.data/IModelSettings.cs
+++ nlp.data/IModelSettings.cs
@@ -5,22 +5,11 @@ namespace nlp.data
 {
     public interface IModelSettings<T>
     {
-        [JsonPropertyName("id")]
         public string Id { get; set; }
-
-        [JsonPropertyName("model")]
         public T Model { get; set; }
-
-        [JsonPropertyName("stopWords")]
         public string[] StopWords { get; set; }
-
-        [JsonPropertyName("stopWordsLength")]
         public int? StopWordsLength { get; }
-
-        [JsonPropertyName("delimiters")]
         public char[] Delimiters { get; set; }
-
-        [JsonPropertyName("publickey")]
         public Guid PublicKey { get; }
     }
 }
