79aa54b411afae01330fdd9877cb8a4373c352e4
gkama <kamacharov.gk@pg.com>
2020-01-06 14:19:31 -0500

    update bulgarian model to not include "language" in the language field

--- nlp.data/Model.Bulgarian.cs
+++ nlp.data/Model.Bulgarian.cs
@@ -8,7 +8,7 @@ namespace nlp.data
     {
         public string Id { get; set; }
         public string Name { get; set; }
-        public string Language => "Български език";
+        public string Language => "Български";
         public string LanguageUs => "Bulgarian";
         public string Details { get; set; }
         public Guid PublicKey => Guid.NewGuid();
