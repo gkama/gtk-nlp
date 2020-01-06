f9277eabbeccb0a0694b362223ed4223f23347aa
Georgi Kamacharov <gkamacharov@gmail.com>
2020-01-05 20:52:17 -0500

    add stemmer unit tests

---
+++ nlp.tests/Stemmer.Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xunit;
+using Moq;
+
+using nlp.data;
+using nlp.services;
+
+namespace nlp.tests
+{
+    public class StemmerTests
+    {
+        private readonly IStemmer _stemmer;
+
+        public StemmerTests()
+        {
+            _stemmer = new Stemmer();
+        }
+
+        [Theory]
+        [InlineData("testing")]
+        [InlineData("word")]
+        [InlineData("serialization")]
+        public void Stem_Valid(string Word)
+        {
+            var stem = _stemmer.Stem(Word);
+
+            Assert.NotNull(stem);
+        }
+
+        [Fact]
+        public void Categorization_Categori()
+        {
+            var stem = _stemmer.Stem("categorization");
+
+            Assert.Equal("categor", stem.Value);
+        }
+    }
+}
