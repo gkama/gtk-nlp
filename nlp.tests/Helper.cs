d3a1c8463c870bd8433d9148ec5d949a66969f1a
gkama <kamacharov.gk@pg.com>
2020-01-13 12:56:29 -0500

    add helper class in the unit tests project to get specific services

---
+++ nlp.tests/Helper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+using nlp.data;
+using nlp.services;
+using nlp.services.text;
+
+namespace nlp.tests
+{
+    public class Helper<T>
+    {
+        private IServiceProvider Provider()
+        {
+            var services = new ServiceCollection();
+
+            services.AddScoped(typeof(INlpRepository<>), typeof(NlpRepository<>));
+            services.AddScoped(typeof(ITextMiningRepository<>), typeof(TextMiningRepository<>));
+            services.AddScoped<IStemmer, Stemmer>();
+            services.AddScoped<ISummarizer, Summarizer>();
+            services.AddSingleton(typeof(Models<>));
+
+            services.AddLogging();
+            services.AddMemoryCache();
+
+            return services.BuildServiceProvider();
+        }
+
+        public T GetService()
+        {
+            return Provider().GetService<T>();
+        }
+    }
+}
