3a95af346ba7055f9ea00410dd1ed2acbdf3839a
Georgi Kamacharov <gkamacharov@gmail.com>
2020-01-17 10:18:21 -0500

    add nlp request class

---
+++ nlp.data/INlpRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nlp.data
+{
+    public interface INlpRequest<T>
+    {
+        public string Content { get; set; }
+        public T Model { get; set; }
+        public string[] Delimiters { get; set; }
+        public string[] StopWords { get; set; }
+        public string ModelId { get; set; }
+        public string ModelName { get; set; }
+        public string ModelDetails { get; set; }
+    }
+}
