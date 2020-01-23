15e40699adffe27751c85881ac4f8f2fcafb5bb9
Georgi Kamacharov <gkamacharov@gmail.com>
2020-01-23 17:45:33 -0500

    add nlp response

---
+++ nlp.data/INlpResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nlp.data
+{
+    public interface INlpResponse
+    {
+    }
+}
---
+++ nlp.data/NlpResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace nlp.data
+{
+    public class NlpResponse : INlpResponse
+    {
+        public bool Summarized { get; set; } = false;
+        public int SummarizedLength { get; set; }
+        public int Length { get; set; }
+        public ICollection<ICategory> Categories { get; set; } = new List<ICategory>();
+    }
+}
