e8727cc20621d91d640b7d728328b8159c7f13cc
gkama <kamacharov.gk@pg.com>
2020-02-20 15:11:02 -0500

    add test yaml string

---
+++ nlp.tests/Configuration.Tests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using Xunit;
+using Moq;
+
+using Microsoft.Extensions.Logging;
+
+using nlp.data;
+using nlp.services.text;
+
+namespace nlp.tests
+{
+    public class ConfigurationTests
+    {
+        private string testYaml =>
+            @"---
+invoice: 34843
+date   : 2001-01-23
+bill-to: &id001
+    given  : Chris
+    family : Dumars
+    address:
+        lines: |
+            458 Walkman Dr.
+            Suite #292
+        city    : Royal Oak
+        state   : MI
+        postal  : 48046
+ship-to: *id001
+product:
+    - sku         : BL394D
+      quantity    : 4
+      description : Basketball
+      price       : 450.00
+    - sku         : BL4438H
+      quantity    : 1
+      description : Super Hoop
+      price       : 2392.00
+tax  : 251.42
+total: 4443.52
+comments:
+    Late afternoon is best.
+    Backup contact is Nancy
+    Billsmer @ 338-4338.
+            ";
+    }
+}
