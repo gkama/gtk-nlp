e7722aa4ca461b61078ba96d46d7194b73bccbe7
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-24 19:03:02 -0500

    add nlp repository model tests

---
+++ nlp.tests/NlpRepository.Model.Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+using Xunit;
+using Moq;
+
+using nlp.data;
+using nlp.services;
+using nlp.services.text;
+
+namespace nlp.tests
+{
+    public class NlpRepositoryModelTests
+    {
+        private readonly INlpRepository<Model> _repo;
+
+        public NlpRepositoryModelTests()
+        {
+            _repo = new Helper<INlpRepository<Model>>().GetService();
+        }
+
+        [Fact]
+        public void GetNlpRequestSchema_Valid()
+        {
+            var schema = _repo.GetNlpRequestSchema();
+
+            Assert.True(schema != null);
+        }
+    }
+}
