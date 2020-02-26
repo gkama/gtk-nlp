5c61b19a284910e79ea268cde1622845136d9c53
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-25 21:18:00 -0500

    add tokenize as public method to unit test

--- nlp.services/INlpRepository.cs
+++ nlp.services/INlpRepository.cs
@@ -10,6 +10,7 @@ namespace nlp.services
     {
         public INlpResponse Categorize(INlpRequest<T> Request, string Id = null);
         public IModelSettings<T> Parse(INlpRequest<T> Request, string Id = null);
+        public IEnumerable<string> Tokenize(string Content, char[] Delimiters, string[] StopWords);
         public object GetNlpRequestSchema();
         public object Sample();
     }
