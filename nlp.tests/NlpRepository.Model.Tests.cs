e6a71e20c1ef8a4b44fdab5b7844765985d86332
Georgi Kamacharov <gkamacharov@gmail.com>
2020-02-25 21:18:08 -0500

    add tokenize unit tests

--- nlp.tests/NlpRepository.Model.Tests.cs
+++ nlp.tests/NlpRepository.Model.Tests.cs
@@ -24,6 +24,17 @@ namespace nlp.tests
             _repo = new Helper<INlpRepository<Model>>().GetService();
         }
 
+        [Theory]
+        [InlineData("this is to try and tokenize this.")]
+        [InlineData("testing, testing, testing")]
+        [InlineData("aaaaaaaaaaaaa, b. c. d. Also this is a sentence.")]
+        public void Tokenize_Valid(string Content)
+        {
+            var tokenized = _repo.Tokenize(Content, new char[] { ',', '.' }, new string[] { "stop", "me" });
+
+            Assert.NotEmpty(tokenized);
+        }
+
         [Fact]
         public void GetNlpRequestSchema_Valid()
         {
